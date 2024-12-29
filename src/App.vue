48bf742a3a7149be11faad2465352fa96c59d6b8
bra1695 <39637298+bra1695@users.noreply.github.com>
2024-12-30 00:34:50 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,84 @@
+<template>
+ <div
+    class="container dm-sans font-jakarta p-3.5 sm:px-4 md:px-8 lg:px-16 xl:px-16 mx-auto my-2"
+  >
+  <div class="grid grid-cols-1 lg:grid-cols-4 lg:grid-rows-4 gap-4">
+  <!-- Left Section (First Column) -->
+  <div class="col-span-1 row-span-4 lg:order-1 order-2 grid grid-rows-2 gap-4">
+    <!-- Create and Schedule -->
+    <div class="bg-[#faeee2] flex flex-col items-center text-center lg:items-start lg:text-left px-6 lg:px-8 py-6 lg:py-8 rounded-xl">
+      <span class="text-3xl lg:text-4xl font-semibold">
+        Create and schedule <br />
+        content <br />
+        <span class="text-[#7452c8]">quicker</span>
+      </span>
+      <img class="mt-4 max-w-full h-auto" src="/images/illustration-create-post.webp" />
+    </div>
+    <!-- AI Content -->
+    <div class="bg-[#ffcc69] flex flex-col items-center text-center lg:items-start lg:text-left px-6 lg:px-8 py-6 lg:py-8 rounded-xl">
+      <span class="text-3xl lg:text-4xl font-semibold">
+        Write your <br />
+        content <br />
+        using AI.
+      </span>
+      <img class="mt-4 max-w-full h-auto" src="/images/illustration-ai-content.webp" />
+    </div>
+  </div>
+
+  <div class="row-span-4 lg:order-2 order-1 col-span-3 grid grid-cols-1 gap-4 lg:grid-cols-3 lg:grid-rows-3">
+  <!-- Card 1 -->
+  <div class="col-span-1 lg:col-span-2 row-span-1 bg-[#7651dc] flex flex-col items-center justify-center px-6 py-6 rounded-xl text-white">
+    <div class="text-4xl lg:text-6xl text-center">
+      Social Media <span class="text-[#f8cb5e]">10x</span>
+    </div>
+    <div class="text-4xl lg:text-6xl text-center">Faster with AI</div>
+    <img class="mt-6" src="/images/illustration-five-stars.webp" alt="Five Stars" />
+    <div class="text-sm lg:text-md font-extralight">Over 4,000 5-star reviews</div>
+  </div>
+
+  <!-- Card 2 -->
+  <div class="col-span-1 row-span-1 lg:row-span-2 bg-[#dcd1fc] flex flex-col px-6 py-6 rounded-xl overflow-hidden">
+    <div class="text-2xl lg:text-3xl">Schedule to <br /> social media</div>
+    <img class="mt-4 lg:mt-6" src="/images/illustration-schedule-posts.webp" alt="Schedule Posts" />
+    <div class="text-sm lg:text-lg mt-8 lg:mt-16 w-full lg:w-48">
+      Optimize post timings to publish content at the perfect time for your audience.
+    </div>
+  </div>
+
+  <!-- Card 3 -->
+  <div class="col-span-1 row-span-1 bg-white flex flex-col justify-center py-4 px-4 items-center rounded-xl overflow-hidden">
+    <img class="mt-4 lg:mt-6" src="/images/illustration-multiple-platforms.webp" alt="Multiple Platforms" />
+    <div class="mt-4 lg:mt-8 text-xl lg:text-3xl text-center">
+      Manage <br /> multiple <br /> accounts and platforms.
+    </div>
+  </div>
+
+  <!-- Card 4 -->
+  <div class="col-span-1 row-span-1 lg:relative bg-white flex flex-col py-4 px-4 rounded-xl overflow-hidden">
+    <div class="text-xl lg:text-3xl">
+      Maintain a <br /> Consistent <br /> Posting <br /> Schedule
+    </div>
+    <img class="mt-16 lg:mt-40 lg:absolute" src="/images/illustration-consistent-schedule.webp" alt="Consistent Schedule" />
+  </div>
+
+  <!-- Card 5 -->
+  <div class="col-span-1 row-span-1 bg-white flex flex-col justify-center py-4 px-4 items-center rounded-xl overflow-hidden">
+    <div class="text-center">
+      <div class="text-4xl lg:text-6xl font-bold">> 56 %</div>
+      <div class="mt-4 lg:mt-8 text-lg">faster audience growth</div>
+    </div>
+    <img class="mt-4 lg:mt-6" src="/images/illustration-audience-growth.webp" alt="Audience Growth" />
+  </div>
+
+  <!-- Card 6 -->
+  <div class="col-span-1 lg:col-span-2 row-span-1 bg-[#7651dc] text-white flex flex-col lg:flex-row justify-center items-center rounded-xl py-6 px-6">
+    <img class="mt-4 lg:mt-0 lg:w-1/2 px-16" src="/images/illustration-grow-followers.webp" alt="Grow Followers" />
+    <div class="py-4 lg:py-16 text-xl lg:text-3xl text-center lg:text-left">
+      Grow Followers with non-stop content
+    </div>
+  </div>
+</div>
+
+</div>
+   </div>
+</template>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// eslint-disable-next-line
+import './styles.css';
+createApp(App).mount('#app')
